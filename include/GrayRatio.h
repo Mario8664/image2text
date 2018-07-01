0d5d292537ade082c8073c878d8ccef1569ac076
NemesissLin <353388639@qq.com>
2018-07-01 23:53:54 +0800

    polish project structure

---
+++ include/GrayRatio.h
@@ -0,0 +1,5 @@
+#ifndef GRAYRATIO_H_
+#define GRAYRATIO_H_
+#include<cstring>
+double CalcGrayRatio(char base, std::string _symbol_path);
+#endif // !GRAYRATIO_H_
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include<iostream>
+#include<GrayRatio.h>
 using namespace std;
 int main(int argc, char const *argv[])
 {
-    cout<<"Relax!"<<endl;
+	//Tested in Windows with VS2017
+    cout<<CalcGrayRatio('9',"D:/image2text/CharGrayRatioCalculator/AllFontSymbol/")<<endl;
+	cin.get();
     return 0;
 }
