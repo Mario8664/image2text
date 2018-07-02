fb162705e421221e04bf715dd81b55beb8c2f78f
LinZong <353388639@qq.com>
2018-07-02 22:48:42 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,15 +9,21 @@ int main(int argc, char const *argv[])
 		this project.
 		
 		VC++ Directory -> Include Directory and Library Directory -> Set the OpenCV path in your computer.
-
-		if you are coding in Linux, unless you had modified the install path of OpenCV, our cmake script
+		
+		Then you need to navigate to the project folder in cmd or powershell, run
+		
+		git rm --cache image2text.vcxproj
+		
+		manually, to pretend the conflicts of the VS configuration for each contributor.(too complex..>_<)
+		
+		if you are coding in Linux, unless you had modified the installation path of OpenCV, our cmake script
 		had been configured all the lib path and include path correctly. Just tap your kb!
 			
 		This is a simple function that convert a colorful photo to gray.
 		if it popups a window normally, means that you have configured all the environment
 		successfully.
 	*/
-	auto Result = ConvertPhotoToGray("F:/CLJ.bmp");
+	Mat Result(600, 800, CV_8UC3);
 	cv::imshow("Result", Result);
 	cv::waitKey();
     return 0;
