fe4642b441e0ef5c4fb567c202ebf7bedef85f0a
NemesissLin <353388639@qq.com>
2018-07-02 22:05:43 +0800

    Fix space mistakes in autoconfig script

--- env/ubuntu.sh
+++ env/ubuntu.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
-echo "Auto configure compile environment..."
- sudo apt-get -y update
- sudo apt-get install -y build-essential
- sudo apt-get install -y cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
- sudo apt-get install -y python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
- curl -sL https://github.com/opencv/opencv/archive/3.4.1.zip > opencv.zip
- unzip opencv.zip
- cd opencv-3.4.1
- mkdir build
- cd build
- cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON ..
+echo "Auto configurae development environment for image2text"
+sudo apt-get -y update
+sudo apt-get -y install build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+echo "Downloading source code of OpenCV"
+curl -sL https://github.com/opencv/opencv/archive/3.4.1.zip > opencv.zip
+unzip opencv.zip
+cd opencv-3.4.1
+mkdir build && cd build
+cmake ..
+make -j8
+sudo make install
+sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
+sudo ldconfig
+echo "OpenCV installed"
 
- make -j4
- sudo make install
-  # Add configuration to OpenCV to tell it where the library files are located on the file system (/usr/local/lib)
- sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
- sudo ldconfig
- echo "OpenCV installed."
- cd ../../..
+cd ../../..
+mkdir bin && cd bin
+cmake ..
+make
 
- mkdir bin && cd bin
- cmake ..
- make
-
- echo "Image2Text build successfully."
+echo "image2text build successfully"
--- include/GrayRatio.h
+++ include/GrayRatio.h
@@ -2,7 +2,6 @@
 #define GRAYRATIO_H_
 #include<cv.h>
 #include<cstring>
-
 double CalcCharGraphGrayRatio(char base, std::string _symbol_path);
 cv::Mat ConvertPhotoToGray(std::string Photo_Path);
 #endif // !GRAYRATIO_H
