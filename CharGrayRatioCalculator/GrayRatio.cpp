b81d9d64c7ba8682a32acdf68aadf8175b8b22d2
Mario8664 <pooh3000@126.com>
2018-07-03 18:06:19 +0800

    alpha to black

--- CharGrayRatioCalculator/GrayRatio.cpp
+++ CharGrayRatioCalculator/GrayRatio.cpp
@@ -42,29 +42,45 @@ double CalcCharGraphGrayRatio(char base,std::string _symbol_path="AllFontSymbol/
 cv::Mat ConvertPhotoToGray(std::string Photo_Path)
 {
 	Mat Original = imread(Photo_Path,-1);//flags <0 is to read the alpha infomation
-	Mat Gray;
+	Mat Gray(Original.size(),3);
 	if (Original.empty())
 	{
 		std::cerr << "Can not open image." << std::endl;
 		exit(1);
 	}
 	cvtColor(Original, Gray, COLOR_RGBA2RGB);
-	//cvtColor(Original, Gray, CV_BGR2GRAY);
+	//cvtColor(Original, Gray, CV_BGRA2RGB);
 	/*
 		This function will return a Mat, a useful image format in OpenCV.
 		You can read the RGB of each pixel like below.
 	*/
+	//for (int i = 0; i < Gray.rows; ++i)
+	//{
+	//	Vec3b *p = Gray.ptr<Vec3b>(i);
+	//	for (int j = 0; j < Gray.cols; ++j)
+	//	{
+	//		std::cout<<(int)p[j][0]<<" "; //Blue
+	//		std::cout << (int)p[j][1] << " "; //Green
+	//		std::cout << (int)p[j][2] << std::endl; //Red
+	//	}
+	//}
 	for (int i = 0; i < Original.rows; ++i)
 	{
 		Vec4b *p = Original.ptr<Vec4b>(i);
+		Vec3b *q = Gray.ptr<Vec3b>(i);
 		for (int j = 0; j < Original.cols; ++j)
 		{
-			p[j][0]; //Blue
-			p[j][1]; //Green
-			p[j][2]; //Red
-			std::cout << (int)p[j][3] << " ";//Alpha
+			q[j][0] = p[j][0] * (bool)p[j][3]; //Blue
+			q[j][1] = p[j][1] * (bool)p[j][3]; //Green
+			q[j][2] = p[j][2] * (bool)p[j][3]; //Red
+			//std::cout << (int)p[j][3] << " ";//Alpha
 		}
 	}
+	std::cout << Gray.channels() << std::endl;
+	//Gray.channels[0] = Original.channels[0] * Original.channels[3];
+	//Gray.channels[1] = Original.channels[1] * Original.channels[3];
+	//Gray.channels[2] = Original.channels[2] * Original.channels[3];
+
 	
 	return Gray;
 }
