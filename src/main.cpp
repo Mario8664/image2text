f2f7d9f269b76525e8eb4ba8e2268ad65c0ae067
林总 <353388639@qq.com>
2018-07-03 14:30:31 +0800

    Fix CMake scripts

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
-#include<GrayRatio.h>
 #include "opencv2/core.hpp"
+#include "opencv2/imgproc.hpp"
 #include "opencv2/highgui.hpp"
+#include "opencv2/videoio.hpp"
+#include "GrayRatio.h"
 using namespace std;
+using namespace cv;
 int main(int argc, char const *argv[])
 {
 	/*
@@ -24,8 +27,8 @@ int main(int argc, char const *argv[])
 		if it popups a window normally, means that you have configured all the environment
 		successfully.
 	*/
-	cv::Mat Result(600, 800, CV_8UC3);
-	cv::imshow("Result", Result);
-	cv::waitKey();
+	Mat Result(600, 800, CV_8UC3);
+	imshow("Result", Result);
+	waitKey();
     return 0;
 }
