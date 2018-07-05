20b1cb538cc0422b67935eb4240fc88134758e5d
林总 <353388639@qq.com>
2018-07-05 22:29:35 +0800

    Finish a web client

---
+++ WebClient/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebImage2Text.Models;
+
+namespace WebImage2Text.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Image2Text()
+        {
+            return View();
+        }
+        [HttpPost("UploadImage")]
+        public IActionResult UploadImage([FromServices]IHostingEnvironment env, List<IFormFile> SourceImage, ImageViewModel Model)
+        { 
+            // full path to file in temp location
+            var filePath = env.WebRootPath + "/upload/"+DateTime.Now.ToString("MMddHHmmss") + "."+ SourceImage.First().FileName.Split('.').Last();
+
+            foreach (var formFile in SourceImage)
+            {
+                if (formFile.Length > 0)
+                {
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        formFile.CopyTo(stream);
+                    }
+                }
+            }
+            string Param = "--source={0} --scalex={1} --scaley={2} --type={3}";
+            Param=String.Format(Param,filePath,Model.ScaleX, Model.ScaleY, 1);
+            ViewData["Result"]=exec("image2text",Param);
+            return View("Result");
+        }
+        public string exec(string exePath, string parameters)
+        {
+            System.Diagnostics.Process pProcess = new System.Diagnostics.Process();
+            pProcess.StartInfo.FileName = exePath;
+            pProcess.StartInfo.Arguments = parameters; //argument
+            pProcess.StartInfo.UseShellExecute = false;
+            pProcess.StartInfo.RedirectStandardOutput = true;
+            pProcess.StartInfo.RedirectStandardError = true;
+            pProcess.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            pProcess.StartInfo.CreateNoWindow = true; //not diplay a windows
+            pProcess.Start();
+            string output = pProcess.StandardOutput.ReadToEnd(); //The output result
+            string errput = pProcess.StandardError.ReadToEnd();
+            pProcess.WaitForExit();
+            return output;
+        }
+    }
+}
---
+++ WebClient/Models/ImageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebImage2Text.Models
+{
+    public class ImageViewModel
+    {
+        [Range(1,int.MaxValue)]
+        public int ScaleX { get; set; }
+        [Range(1, int.MaxValue)]
+        public int ScaleY { get; set; }
+        public string UploadedFileName { get; set; }
+        [Required]
+        [Display(Name = "Image to upload")]
+        [FileExtensions(Extensions = ".jpg,.png,.bmp", ErrorMessage = "Error image format.")]
+        public IFormFile SourceImage { get; set; }
+    }
+}
